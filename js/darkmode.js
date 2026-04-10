ae6a96a3914b11a41dbc41f9db154a2f9f110da4
BEGRAFX <brian@pixelsnstuff.com>
2026-04-10 13:05:04 -0400

    0.1.3 update of info.yml, darkmode.js libraries.yml and .theme files

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,16 @@
-
-function toggleDarkMode() {
-  if (window.halfmoon) {
-    halfmoon.toggleDarkMode();
-  }
-}
+(function (Drupal, once) {
+  Drupal.behaviors.desertMoonDarkMode = {
+    attach: function (context) {
+      once('dark-mode-toggle', context.querySelectorAll('#dark-mode-toggle'))
+        .forEach(function (btn) {
+          btn.addEventListener('click', function () {
+            if (window.halfmoon) {
+              window.halfmoon.toggleDarkMode();
+            } else {
+              console.warn('Halfmoon not loaded');
+            }
+          });
+        });
+    }
+  };
+})(Drupal, once);
